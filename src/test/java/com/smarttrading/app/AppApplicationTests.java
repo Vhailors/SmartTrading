d0f533c3c28387b29b599bf2210682852d12f720
Vhaily <scyllacharybdis33@gmail.com>
2023-03-05 09:24:55 +0100

    Initial commit

---
+++ src/test/java/com/smarttrading/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttrading.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
